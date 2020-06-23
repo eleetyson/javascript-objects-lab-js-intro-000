52328b956fbe54d62c06aa4cb627ffca8de6ca47
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-23 18:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let recipes = {type:"pasta", ingredient:"flour"};
 
 function updateObjectWithKeyAndValue(recipes, keyAdd, valueAdd) {
   recipes.keyAdd = valueAdd;
+  console.log(recipes);
   return recipes;
 }
