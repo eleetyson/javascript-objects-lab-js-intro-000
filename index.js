ff21bd09928815b0194109211ec3b66a8878f583
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-23 18:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let recipes1 = {type:"pasta", ingredient:"flour"};
 
 // returns object with the original key value pairs and the new key value pair
 function updateObjectWithKeyAndValue(recipes, keyAdd, valueAdd) {
-  return Object.assign({}, ...recipes, {[keyAdd]:valueAdd});
+  return Object.assign({}, recipes, {[keyAdd]:valueAdd});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, keyAdd, valueAdd) {
